090df8270014f6e2716c0b98cf3343acf1222262
noiqum <onurcoskun99@gmail.com>
2024-03-17 21:04:14 +0300

    chore: head config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000, immutable',
+                    },
+                ],
+            },
+            {
+                source: '/',
+                headers: [
+                    {
+                        key: 'x-custom-header',
+                        value: 'Fidelle',
+                    },
+                ],
+            },
+
+            {
+                source: '/About',
+                headers: [
+                    {
+                        key: 'x-custom-header',
+                        value: 'Fidelle Story',
+                    },
+                ],
+            },
+        ];
+    }
+};
 
 export default nextConfig;
