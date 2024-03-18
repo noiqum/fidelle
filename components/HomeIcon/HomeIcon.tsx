e37fa10d3d30cbbe587b847382dc070d674db6ce
noiqum <onurcoskun99@gmail.com>
2024-03-18 22:40:16 +0300

    feat: home icon redirect to homepage

--- components/HomeIcon/HomeIcon.tsx
+++ components/HomeIcon/HomeIcon.tsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import "./HomeIcon.scss";
 export default function HomeIcon() {
   return (
-    <div className="HomeIcon">
+    <Link href="/" className="HomeIcon">
       <div className="HomeIcon__Line"></div>
       <div className="HomeIcon__Line"></div>
       <div className="HomeIcon__Line"></div>
-    </div>
+    </Link>
   );
 }
