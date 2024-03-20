7c26bfa24e6cb14c010bc683077c09a955d246ac
noiqum <onurcoskun99@gmail.com>
2024-03-20 16:15:33 +0300

    feat: navlink base

--- components/NavLink/NavLink.tsx
+++ components/NavLink/NavLink.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import "./NavLink.scss";
+import UserIcon from "../../public/assets/svg/user.svg";
+import CartIcon from "../../public/assets/svg/cart.svg";
+import Image from "next/image";
 type NavLinkType = "auth" | "cart";
 interface Props {
   type: NavLinkType;
@@ -9,10 +12,15 @@ const href = {
   auth: "/auth",
   cart: "/cart",
 };
+const icon = {
+  auth: UserIcon,
+  cart: CartIcon,
+};
 
 export default function NavLink({ type }: Props) {
   return (
     <Link href={href[type]} className="NavLink">
+      <Image width={32} height={32} src={icon[type]} alt={type}></Image>
       {type}
     </Link>
   );
